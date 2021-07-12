2a96336e83cd12db706c4dcec3feed5cfcb5232a
jchen929 <jishengchen3120@gatech.edu>
2021-07-11 23:16:10 -0400

    update

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    // host : 'localhost',
-    user : 'postgres',
-    password : 'Song8986778#',
-    database : 'smart_brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
